9dba19d895149fbba029f9d4691670b6ac66b53d
jav974 <jeremyvienne@gmail.com>
2024-06-11 14:21:04 +0200

    updated symfony versions for tests and added php 8.3

--- src/Cache/ResourcesWarmer.php
+++ src/Cache/ResourcesWarmer.php
@@ -4,35 +4,70 @@ namespace Jav\ApiTopiaBundle\Cache;
 
 use Jav\ApiTopiaBundle\GraphQL\ResourceLoader;
 use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
-class ResourcesWarmer implements CacheWarmerInterface
-{
-    /** @var array<string, mixed> */
-    private array $config;
-
-    public function __construct(private readonly ResourceLoader $resourceLoader)
+if (Kernel::VERSION_ID < 7000) {
+    class ResourcesWarmer implements CacheWarmerInterface
     {
-    }
+        /** @var array<string, mixed> */
+        private array $config;
 
-    /**
-     * @param array<string, mixed> $config
-     */
-    public function setConfig(array $config): void
-    {
-        $this->config = $config;
-    }
-
-    public function isOptional(): bool
-    {
-        return false;
-    }
-
-    public function warmUp(string $cacheDir): array
-    {
-        foreach ($this->config as $schemaName => $config) {
-            $this->resourceLoader->loadResources($schemaName, $config['resource_directories'], true);
+        public function __construct(private readonly ResourceLoader $resourceLoader)
+        {
         }
 
-        return [];
+        /**
+         * @param array<string, mixed> $config
+         */
+        public function setConfig(array $config): void
+        {
+            $this->config = $config;
+        }
+
+        public function isOptional(): bool
+        {
+            return false;
+        }
+
+        public function warmUp(string $cacheDir): array
+        {
+            foreach ($this->config as $schemaName => $config) {
+                $this->resourceLoader->loadResources($schemaName, $config['resource_directories'], true);
+            }
+
+            return [];
+        }
+    }
+} else {
+    class ResourcesWarmer implements CacheWarmerInterface
+    {
+        /** @var array<string, mixed> */
+        private array $config;
+
+        public function __construct(private readonly ResourceLoader $resourceLoader)
+        {
+        }
+
+        /**
+         * @param array<string, mixed> $config
+         */
+        public function setConfig(array $config): void
+        {
+            $this->config = $config;
+        }
+
+        public function isOptional(): bool
+        {
+            return false;
+        }
+
+        public function warmUp(string $cacheDir, ?string $buildDir = null): array
+        {
+            foreach ($this->config as $schemaName => $config) {
+                $this->resourceLoader->loadResources($schemaName, $config['resource_directories'], true);
+            }
+
+            return [];
+        }
     }
 }
